4334c8af293f2d0bfb3a5f63669f24abe3675234
Adenilson <adenilson.sales@outlook.com>
2026-08-29 19:15:23 -0300

    Implementando configuração de SEO.

--- frontend/js/catalogo.js
+++ frontend/js/catalogo.js
@@ -31,7 +31,7 @@ function criarCardHTML(item) {
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h1 class="modal-title fs-5">${item.title}</h1>
+                                <h2 class="modal-title fs-5">${item.title}</h2>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
